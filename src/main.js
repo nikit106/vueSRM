0ff2266ea05473a5877d4ee80fe1e54be289a10b
Nikita Komarov <nikit106@gmail.com>
2020-02-17 00:45:31 +0300

    end categories create and edit

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import dateFilter from '@/filters/date.filter'
+import currencyFilter from '@/filters/currency.filter'
 import messagePlugin from '@/utils/message.plugin'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
+import Loader from '@/components/app/Loader'
 
 import firebase from 'firebase/app'
 
@@ -19,10 +21,21 @@ Vue.config.productionTip = false
 Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
+Vue.filter('currency', currencyFilter)
+Vue.component('Loader', Loader)
 
 
 
-
+firebase.initializeApp({
+  apiKey: "AIzaSyDITJfDgEW4jFWqykJgFvxwim_725IJUrk",
+  authDomain: "vuesrm.firebaseapp.com",
+  databaseURL: "https://vuesrm.firebaseio.com",
+  projectId: "vuesrm",
+  storageBucket: "vuesrm.appspot.com",
+  messagingSenderId: "940321209047",
+  appId: "1:940321209047:web:ad6db139e2123b51da0e92",
+  measurementId: "G-9Y0E1LWXKH"
+})
 
 let app
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import auth from './auth'
+import info from './info'
+import category from './category'
 
 Vue.use(Vuex)
 
@@ -16,10 +18,18 @@ export default new Vuex.Store({
       state.error = null
     }
   },
+  actions: {
+    async fetchCurrency() {
+      //const key = process.env.VUE_APP_FIXER
+      const key = '3143cfd45a95c7259c2c1f532f80b630'
+      const res = await fetch(`http://data.fixer.io/api/latest?access_key=${key}&symbols=USD,EUR,RUB`)
+      return await res.json()
+    }
+  },
   getters: {
     error: s => s.error
   },
   modules: {
-    auth
+    auth, info, category
   }
 })
